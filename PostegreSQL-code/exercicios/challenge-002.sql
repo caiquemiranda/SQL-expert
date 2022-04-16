3d43ef69dc4c3330e25ff6620a7fe77ec3360a0c
caiquemiranda <caiquemirandas@gmail.com>
2022-04-16 19:53:30 -0300

    challenge

--- PostegreSQL-code/exercicios/challenge-002.sql
+++ PostegreSQL-code/exercicios/challenge-002.sql
@@ -20,19 +20,43 @@ FROM sales.customers
 -- (Exercício 3) Selecione todas as informações do cliente mais rico da tabela sales.customers
 -- (possívelmente a resposta contém mais de um cliente)
 
-
+SELECT *
+FROM sales.customers
+WHERE income = (
+	SELECT MAX(income) 
+	FROM sales.customers
+    )
 
 -- (Exercício 4) Conte quantos veículos de cada marca tem registrado na tabela sales.products
 -- Ordene o resultado pelo nome da marca
 
-
+SELECT 
+	brand, 
+	COUNT(*) AS contagem
+FROM sales.products
+GROUP BY brand
+ORDER BY contagem DESC
 
 -- (Exercício 5) Conte quantos veículos existem registrados na tabela sales.products
 -- por marca e ano do modelo. Ordene pela nome da marca e pelo ano do veículo
 
-
+SELECT 
+	brand,
+	model_year,
+	COUNT(*) AS contagem
+	
+FROM sales.products
+GROUP BY brand ,model_year
+ORDER BY brand, contagem DESC
 
 -- (Exercício 6) Conte quantos veículos de cada marca tem registrado na tabela sales.products
 -- e mostre apenas as marcas que contém mais de 10 veículos registrados
 
-
+SELECT 
+	brand,
+	COUNT(*) AS contagem
+	
+FROM sales.products
+GROUP BY brand
+HAVING COUNT(*) > 10
+ORDER BY contagem DESC
